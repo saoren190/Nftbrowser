d4b0e54b8352d56c3a98d9a98779c69997a7f33c
wangq <wangquanze922@gmail.com>
2024-02-17 15:58:36 -0600

    update

--- src/components/ContractTrades.js
+++ src/components/ContractTrades.js
@@ -11,37 +11,80 @@ import {
   Tooltip as ChartTooltip,
 } from "recharts";
 
+
 const ratio = 6900000000000000 / 1300;
 
-const ModalContent = ({tokenAddress}) => {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
 
-    useEffect(() => {
-        getContractTrades(tokenAddress)
-          .then((resp) => {
-            setData(
-              resp.result.reverse().map((item) => {
-                return {
-                  ...item,
-                  price: Math.floor(Number(item.price) / ratio),
-                  block_timestamp: new Date(
-                    item.block_timestamp
-                  ).toLocaleDateString(),
-                };
-              })
-            );
+const ModalContent = ({ tokenAddress }) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    getContractTrades(tokenAddress)
+      .then((resp) => {
+        setData(
+          resp.result.reverse().map((item) => {
+            return {
+              ...item,
+              price: Math.floor(Number(item.price) / ratio),
+              block_timestamp: new Date(
+                item.block_timestamp
+              ).toLocaleDateString(),
+            };
           })
-          .finally(() => {
-            setLoading(false);
-          });
-      }, []);
-    
-      if (loading) {
-        return <Skeleton active />
-      }
+        );
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-      return ();
+
+  if (loading) {
+    return <Skeleton active />;
+  }
+
+
+  return (
+    <LineChart width={800} height={600} data={data} margin={{ left: 120 }}>
+      <Line type="monotone" dataKey="price" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="block_timestamp" />
+      <YAxis />
+      <ChartTooltip />
+    </LineChart>
+  );
+};
+
+
+const ContractTrades = ({ tokenAddress }) => {
+  const [modalOpen, setModalOpen] = useState();
+
+
+  return (
+    <>
+      <Tooltip title="Trade(s) in this contract">
+        <Button
+          disabled={tokenAddress === ""}
+          style={{ border: "none" }}
+          shape="circle"
+          icon={<TrademarkOutlined />}
+          onClick={() => setModalOpen(true)}
+        />
+      </Tooltip>
+      <Modal
+        width={1000}
+        title="Trade(s) Chart"
+        destroyOnClose
+        open={modalOpen}
+        footer={null}
+        onCancel={() => setModalOpen(false)}
+      >
+        <ModalContent tokenAddress={tokenAddress} />
+      </Modal>
+    </>
+  );
 };
 
 
